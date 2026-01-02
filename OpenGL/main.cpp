b6ec5ee18bdc2cd405d0099ba88b3b91673d598c
Ivan Anisovich <148715019+kateshiAIV@users.noreply.github.com>
2026-01-03 00:44:06 +0100

    live drawing (color depth)

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -16,6 +16,7 @@ using namespace std;
 float CAMERA_DISTANCE = 5.0f;
 #define ASSET_PATH "C:/Users/kishk/source/repos/OpenGL/";
 
+
 //логика коллбека для колесика мыши
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
@@ -36,6 +37,44 @@ void drawPointCloud(const vector<Vertex>& vertices)
 	}
 	glEnd();
 }
+
+void createPointCloud(vector<Vertex>& vertices, string colorImagePath, string depthImagePath)
+{
+	float scale = 10.0f;
+	vertices.clear();
+
+	int cw, ch, cc;
+	int dw, dh, dc;
+
+	unsigned char* colorData = stbi_load(colorImagePath.c_str(), &cw, &ch, &cc, 3);
+	unsigned char* depthData = stbi_load(depthImagePath.c_str(), &dw, &dh, &dc, 1);
+
+	for (int i = 0; i < ch; i++)
+	{
+		for (int j = 0; j < cw; j++)
+		{
+			float depth = depthData[i * cw + j] / 255.0f * scale;
+			float x = (j - cw / 2.0f) / cw * scale;
+			float y = (ch / 2.0f - i) / ch * scale;
+			float z = depth;
+
+			int idx = (i * cw + j) * 3;
+			float r = colorData[idx] / 255.0f;
+			float g = colorData[idx + 1] / 255.0f;
+			float b = colorData[idx + 2] / 255.0f;
+
+
+			//if (depth >= 0.2 && depth <= 1.0) // Фильтрация по глубине
+			if (true)
+			{
+				vertices.push_back({ x, y, z, r, g, b });
+			}
+		}
+	}
+	stbi_image_free(colorData);
+	stbi_image_free(depthData);
+}
+
 
 int main() 
 {
@@ -67,54 +106,13 @@ int main()
 	glPointSize(2.0f);
 
 
-	int cw, ch, cc;
-	int dw, dh, dc;
 
-	string colorImagePath = "C:/Users/kishk/source/repos/OpenGL/OpenGL/assets/color.jpeg";
-	string depthImagePath = "C:/Users/kishk/source/repos/OpenGL/OpenGL/assets/depth.png";
-
-	unsigned char* colorData = stbi_load(colorImagePath.c_str(), &cw, &ch, &cc, 3);
-    unsigned char* depthData = stbi_load(depthImagePath.c_str(), &dw, &dh, &dc, 1);
-
-	if (!colorData)
-	{
-		cout << "Failed to load color image" << endl;
-		return -1;
-	}
-
-	if (!depthData)
-	{
-		cout << "Failed to load depth image" << endl;
-		return -1;
-	}
 
 	vector<Vertex> vertices;
-	float scale = 10.0f;
-	for (int i = 0; i < ch; i++)
-	{
-		for (int j = 0; j < cw; j++)
-		{
-			float depth = depthData[i * cw + j] / 255.0f * scale;
-			float x = (j - cw / 2.0f) / cw * scale;
-			float y = (ch / 2.0f - i) / ch * scale;
-			float z = depth;
+	string colorImagePath = "C:/Users/kishk/source/repos/OpenGL/OpenGL/assets/color.jpeg";
+	string depthImagePath = "C:/Users/kishk/source/repos/OpenGL/OpenGL/assets/depth.png";
+	createPointCloud(vertices, colorImagePath, depthImagePath);
 
-			int idx = (i * cw + j) * 3;
-			float r = colorData[idx] / 255.0f;
-			float g = colorData[idx + 1] / 255.0f;
-			float b = colorData[idx + 2] / 255.0f;
-
-
-			//if (depth >= 0.2 && depth <= 1.0) // Фильтрация по глубине
-			if(true)
-			{
-				vertices.push_back({ x, y, z, r, g, b });
-			}
-		}
-	}
-
-	stbi_image_free(colorData);
-	stbi_image_free(depthData);
 
 
 	while (!glfwWindowShouldClose(window))
@@ -122,8 +120,9 @@ int main()
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
-		glClearColor(0.05f, 0.05f, 0.08f, 1.0f);
+		glClearColor(0.05f, 0.05f, 0.08f, 1.0f); //bg
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
@@ -133,7 +132,10 @@ int main()
 		glLoadIdentity();
 		glTranslatef(0.0f, 0.0f, -CAMERA_DISTANCE);
 
+		// TODO Kinnect data rendering
 
+
+		createPointCloud(vertices, colorImagePath, depthImagePath);
 		drawPointCloud(vertices);
 
 		glfwSwapBuffers(window);
@@ -144,21 +146,3 @@ int main()
 	return 0;
 }
 
-//int main() {
-//
-
-//
-//        glBegin(GL_POINTS);
-//        for (auto& v : vertices) {
-//            glColor3f(v.r, v.g, v.b);
-//            glVertex3f(v.x, v.y, v.z);
-//        }
-//        glEnd();
-//
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    glfwTerminate();
-//    return 0;
-//}
